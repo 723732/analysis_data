19a1fb1059e48afbf51170d4e08b44f0fcb498cb
yxh <723732636@qq.com>
2019-07-03 11:01:20 +0800

    save

--- collateData.py
+++ collateData.py
@@ -35,7 +35,7 @@ def collectResult(testrepairPath, excelFile):
 
 
 def collectResult1(testrepairPath):
-    i = 0
+    i = 1
     workbook = openpyxl.Workbook()
     data_sheet = workbook.create_sheet('result')
     for root, dirs, files in os.walk(testrepairPath):
@@ -77,10 +77,30 @@ def collectallVariants(OnetestrepairPathFile):
             collectVariants(line)
 
 
+def collectrightTest(allrepaircasesPath, repairSubjectsPath):
+    #提取包含正确测试用例的文件
+    with open(allrepaircasesPath) as file_object:
+        for line in file_object:
+            path = line.replace('\t', '\\')
+            path1 = path.split('-vs-')[0]
+            path2 = path.split('-vs-')[1]
+            path2_1 = path2.split('\\')[1]
+            path2_2 = path2_1.replace('.', '\\')
+            path2_3 = path2_2.split('\\')[-1]
+            path2_4 = path2_2.replace('\\' + path2_3, '.java')
+            targetPath = repairSubjectsPath + '\\' + path1 + '\\test\\' + path2_4
+            finalfileName = path1 + '\\test\\' + path2_4
+            finalfileName = finalfileName.replace('\\', '_')
+            finalfileName = 'C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\data-summary\\correct-test\\' + finalfileName
+            if not os.path.exists(finalfileName):
+                shutil.copyfile(targetPath, finalfileName)
+
+
 if __name__ == '__main__':
     # collectRepairCases('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\repair-subjects\\all-repair-cases.txt', 'C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\data.xlsx')
     # collectResult('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\test-repair-author-0625-2min', 'C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\data.xlsx')
-    # collectResult1('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\test-repair-me-0624-2min')
-    # collectVariants('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\test-repair-me-0624-2min\\Gson\\gson-1.5-vs-gson-1.3\\com_google_gson_FunctionWithInternalDependenciesTest_testAnonymousLocalClassesSerialization')
+    # collectResult1('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\test-repair-me-0702-2min')
+    #  collectVariants('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\test-repair-me-0627-2min\\Gson\\gson-1.3-vs-gson-1.2\\com_google_gson_JsonEscapingVisitorTest_testStringArrayVisitationEscapingRequired')
     # collectOnetestrepairPath('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\test-repair-me-0624-2min')
-    collectallVariants('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\test-repair-me-0624-2min\\onetestrepairPath.txt')
+    # collectallVariants('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\test-repair-me-0624-2min\\onetestrepairPath.txt')
+    collectrightTest('C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\repair-subjects\\all-repair-cases.txt', 'C:\\Users\\Y_sun\\Desktop\\2019-1\\experiment-data\\repair-subjects')
